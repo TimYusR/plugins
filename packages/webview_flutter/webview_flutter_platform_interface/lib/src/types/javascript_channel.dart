df452b7957f2ab8a2c27d2b5f9f78414728494f0
Timur Yusupov <timur.yusupov@dualbootpartners.com>
2021-11-12 16:46:00 +0400

    allowed slash in channel names

--- packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/javascript_channel.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/javascript_channel.dart
@@ -7,7 +7,7 @@ import 'javascript_message.dart';
 /// Callback type for handling messages sent from JavaScript running in a web view.
 typedef void JavascriptMessageHandler(JavascriptMessage message);
 
-final RegExp _validChannelNames = RegExp('^[a-zA-Z_][a-zA-Z0-9_]*\$');
+final RegExp _validChannelNames = RegExp('^[a-zA-Z_][a-zA-Z0-9_\/]*\$');
 
 /// A named channel for receiving messaged from JavaScript code running inside a web view.
 class JavascriptChannel {
